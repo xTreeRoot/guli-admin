4e2716fe40246d6e8bcc7c709829f40dbee66bd1
xugenyin <xuxian.xgy@qq.com>
2022-03-20 23:02:57 +0800

    课程修改

--- guli-admin/src/api/edu/course.js
+++ guli-admin/src/api/edu/course.js
@@ -16,5 +16,20 @@ export default {
       url:`/eduservice/teacher/findAll`,
       method:`get`
     })
+  },
+//根据课程id查询课程基本信息
+  getCourseInfoId(id){
+    return request({
+      url:`/eduservice/course/getCourseInfo/${id}`,
+      method:'get'
+    })
+  },
+  //修改课程信息
+  updateCourseInfoId(courseInfo){
+    return request({
+      url:`/eduservice/course/updateCourseInfo`,
+      method:'post',
+      data:courseInfo
+    })
   }
 }
