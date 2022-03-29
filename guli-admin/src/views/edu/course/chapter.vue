fa47dd495a39459f0d226d8e0a91a981b4e676be
tree_root <20081630234@stu.ccit.js.cn>
2022-03-29 10:38:32 +0800

    阿里云点播上传视频
    Signed-off-by: tree_root <20081630234@stu.ccit.js.cn>

--- guli-admin/src/views/edu/course/chapter.vue
+++ guli-admin/src/views/edu/course/chapter.vue
@@ -72,9 +72,29 @@
             </el-radio-group>
           </el-form-item>
           <el-form-item label="上传视频">
-            <!-- TODO -->
+            <el-upload
+              :on-success="handleVodUploadSuccess"
+              :on-remove="handleVodRemove"
+              :before-remove="beforeVodRemove"
+              :on-exceed="handleUploadExceed"
+              :file-list="fileList"
+              :action="BASE_API+'/eduvod/video/uploadAlyiVideo'"
+              :limit="1"
+              class="upload-demo">
+              <el-button size="small" type="primary">上传视频</el-button>
+              <el-tooltip placement="right-end">
+                <div slot="content">最大支持1G，<br>
+                  支持3GP、ASF、AVI、DAT、DV、FLV、F4V、<br>
+                  GIF、M2T、M4V、MJ2、MJPEG、MKV、MOV、MP4、<br>
+                  MPE、MPG、MPEG、MTS、OGG、QT、RM、RMVB、<br>
+                  SWF、TS、VOB、WMV、WEBM 等视频格式上传
+                </div>
+                <i class="el-icon-question"/>
+              </el-tooltip>
+            </el-upload>
           </el-form-item>
         </el-form>
+
         <div slot="footer" class="dialog-footer">
           <el-button @click="dialogVideoFormVisible = false">取 消</el-button>
           <el-button :disabled="saveVideoBtnDisabled" type="primary"
@@ -105,10 +125,13 @@ export default {
         title: '',
         sort: 0,
         free: 0,
-        videoSourceId: ''
+        videoSourceId: '',
+        videoOriginalName: ''//视频名称
       },//封装小节属性
       dialogChapterFormVisible: false,//章节弹框
-      dialogVideoFormVisible: false//小节弹框
+      dialogVideoFormVisible: false,//小节弹框
+      fileList: [],//上传文件列表
+      BASE_API: process.env.BASE_API // 接口API地址
 
 
     }
@@ -123,9 +146,28 @@ export default {
 
   },
   methods: {
+    //点击确定调用的方法
+    handleVodRemove(){
+    //  调用接口的删除
+    },
+    //点击x调用这个方法
+    beforeVodRemove(file,fileList){
+      return this.$confirm(`确定移除 ${ file.name }?`)
+    },
+
+    //成功回调
+    handleVodUploadSuccess(response, file, fileList) {
+      this.video.videoSourceId = response.data.videoId
+      //上传视频名称赋值
+      this .video. videoOriginalName = file.name
+    },
+//视图上传多于一个视频
+    handleUploadExceed(files, fileList) {
+      this.$message.warning('想要重新上传视频，请先删除已上传的视频')
+    },
     // =====================================小节操作 =====================================
     //删除小节
-    removeVideo(id){
+    removeVideo(id) {
       this.$confirm('此操作将永久删除讲该师小节, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
@@ -151,7 +193,7 @@ export default {
       //  弹框
       this.dialogVideoFormVisible = true,
         //    设置章节id
-      this.video.chapterId = chapterId
+        this.video.chapterId = chapterId
       this.video.title = ''
       this.video.sort = 0
       this.video.free = 0
